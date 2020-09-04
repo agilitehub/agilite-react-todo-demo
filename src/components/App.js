0cbd8941cab5b056976cce6d48263b4527c05586
Armand Smit <armand@agilite.io>
2020-09-04 11:29:24 +0200

    Adhoc fix

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { AgiliteReact } from 'armand-testing-typescript'
+import { AgiliteReact } from 'agilite-react'
 
 function App () {
   const appConfig = useSelector(state => state.appConfig)
